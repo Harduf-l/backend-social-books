eaf8806f3e992190a55d818c0f7507c18d7e172e
harduf <harduf93@gmail.com>
2022-01-31 16:39:46 +0200

    starts to build quotes data

---
+++ data/dailyQuotes.js
@@ -0,0 +1,354 @@
+exports.quotesHeb = [
+  {
+    line: "בצאתי מהבית, אני מייחל תמיד להתרחשות שתהפוך את חיי. אני מחכה לה עד שובי. על שום כך איני נשאר אף פעם בחדרי. לדאבוני, מעולם לא התרחש בחיי דבר.",
+    book: "החברים שלי",
+    author: "עמנואל בוב",
+  },
+  {
+    line: "מי שהאמת עימו הוא הרבים, ואפילו הוא לבדו",
+    book: "",
+    author: 'ש"י עגנון',
+  },
+  {
+    line: "אם משהו עומד לקרות לי, אני רוצה להיות שם",
+    book: "הזר",
+    author: "אלבר קאמי",
+  },
+  {
+    line: "אין טעם לחזור לאתמול, מכיוון שהייתי אדם שונה אז.",
+    book: "אליס בארץ הפלאות",
+    author: "לואיס קרול",
+  },
+  {
+    line: "אולי לא הייתי בטוח לגבי מה אכן עניין אותי, אבל הייתי בטוח לחלוטין לגבי מה שלא.",
+    book: "הזר",
+    author: "אלבר קאמי",
+  },
+  {
+    line: "אני לבד בין קולותיהם השמחים, ההגיוניים. כל היצורים הללו מבלים את זמנם בלהסביר, להבין בשמחה שהם מסכימים זה עם זה. בשם אלוהים, מדוע זה כה חשוב לחשוב את אותם דברים כולם יחד?",
+    book: "הבחילה",
+    author: "ז'אן-פול סארטר",
+  },
+  {
+    line: "מילאתי שלוש מחברות בניסיון להבין אם היו אלה הם או רק אני.",
+    book: "משהו כיפי לכאורה שלא אחזור עליו לעולם",
+    author: "דייוויד פוסטר וואלאס",
+  },
+  {
+    line: "אין שפות מתות, רק מוחות רדומים",
+    book: "צלה של הרוח",
+    author: "קרלוס רואיס סאפון",
+  },
+  {
+    line: "הם מכרו את המפתח ליקום לאנשים שאפילו לא ידעו שהוא נעול.",
+    book: "טרי פראצ'ט",
+    author: "בני החורין הקטנים",
+  },
+  {
+    line: "גם אם איחשד בבורות או בצרות אופקים, אני מודה - אני אנאלפבית. קשה לי להעלות בדעתי כיצד עיון בספר יקדם את ענייני.",
+    book: "הכלב היהודי",
+    author: "אשר קרביץ",
+  },
+  {
+    line: "ענן אינו יודע מדוע הוא נע בכיוון שכזה ובמהירות שכזו, כורח פנימי אומר לו לאן ללכת ומתי. אך השמיים יודעים את התכלית העליונה שמעבר לכל העננים. גם אתה תדע ביום בו תגביה עוף עד מעבר לאופק.",
+    book: "ריצ'ארד באך",
+    author: "תעתועים",
+  },
+  {
+    line: "",
+    book: "",
+    author: "",
+  },
+  {
+    line: "",
+    book: "",
+    author: "",
+  },
+  {
+    line: "",
+    book: "",
+    author: "",
+  },
+];
+
+exports.quotesEng = [
+  {
+    line: "Home isn’t where you land; home is where you launch. You can’t pick your home any more than you can choose your family. In poker, you get five cards. Three of them you can swap out, but two are yours to keep: family and native land.",
+    book: "An American Marriage",
+    author: "Tayari Jones",
+  },
+  {
+    line: "To choose doubt as a philosophy of life is akin to choosing immobility as a means of transportation.",
+    book: "Life of Pi",
+    author: "Yann Martel",
+  },
+  {
+    line: "The gods grow jealous of too much contentment anywhere, and they show their displeasure all of a sudden.",
+    book: "Malgudi Days",
+    author: "R. K. Narayan",
+  },
+  {
+    line: "I was within and without, simultaneously enchanted and repelled by the inexhaustible variety of life",
+    book: "The Great Gatsby",
+    author: "F Scott Fitzgerald",
+  },
+  {
+    line: "So I'll do that, and I'll do my best and if my best isn't good enough, at least I will have done everything I could, everything that is in me. I don't have to try to be someone else, someone I could never be.",
+    book: "Abhorsen",
+    author: "Garth Nix",
+  },
+  {
+    line: "A mind needs books like a sword needs a whetstone, if it is to keep its edge.",
+    book: "A Game of Thrones: A Song of Ice and Fire",
+    author: "George R. R. Martin",
+  },
+
+  {
+    line: "Whatever our souls are made of, his and mine are the same.",
+    book: "After We Collided",
+    author: "Anna Todd",
+  },
+  {
+    line: "For the two of us, home isn't a place. It is a person. And we are finally home.",
+    book: "Anna and the French Kiss",
+    author: "Stephanie Perkins",
+  },
+  {
+    line: "It does not do to dwell on dreams and forget to live.",
+    book: "Harry Potter and the Sorcerer’s Stone",
+    author: "J.K. Rowling",
+  },
+  {
+    line: "There is nothing either good or bad, but thinking makes it so.",
+    book: "Hamlet",
+    author: "William Shakespeare",
+  },
+  {
+    line: "It moves at its own measured pace, for it has no reason to hurry. Tomorrow will come in its own good time.",
+    book: "The Sky is Falling",
+    author: "Sidney Sheldon",
+  },
+  {
+    line: "Men go to far greater lengths to avoid what they fear than to obtain what they desire.",
+    book: "The Da Vinci Code",
+    author: "Dan Brown",
+  },
+  {
+    line: "You do not have to struggle to reach God, but you do have to struggle to tear away the self-created veil that hides him from you",
+    book: "Autobiography of a Yogi",
+    author: "Paramahansa Yogananda",
+  },
+  {
+    line: "Never forget what you are, for surely the world will not. Make it your strength. Then it can never be your weakness.",
+    book: "A Song of Ice and Fire",
+    author: "George R.R. Martin",
+  },
+  {
+    line: "All we have to decide is what to do with the time that is given us.",
+    book: "The Fellowship of the Ring",
+    author: "J.R.R. Tolkein",
+  },
+  {
+    line: "I declare after all there is no enjoyment like reading! How much sooner one tires of any thing than of a book! -- When I have a house of my own, I shall be miserable if I have not an excellent library.",
+    book: "Pride and Prejudice",
+    author: "Jane Austen",
+  },
+  {
+    line: "Tomorrow I’ll think of some way to get him back. After all, tomorrow is another day.",
+    book: "Margaret Mitchell",
+    author: "Gone with the Wind",
+  },
+  {
+    line: "Why, sometimes, I’ve believed as many as six impossible things before breakfast.",
+    book: "Lewis Carroll",
+    author: "Through the Looking-Glass",
+  },
+  {
+    line: "Don’t ever tell anybody anything. If you do, you start missing everybody.",
+    book: "J. D. Salinger",
+    author: "The Catcher in the Rye",
+  },
+  {
+    line: "It does not do to dwell on dreams and forget to live.",
+    book: "J.K. Rowling",
+    author: "Harry Potter and the Sorcerer’s Stone",
+  },
+  {
+    line: "You pierce my soul. I am half agony. Half hope. Tell me not that I am too late, that such precious feelings are gone for ever.",
+    book: " Jane Austen",
+    author: "Persuasion",
+  },
+  {
+    line: "I had the epiphany that laughter was light, and light was laughter, and that this was the secret of the universe.",
+    book: " Donna Tartt",
+    author: "The Goldfinch",
+  },
+  {
+    line: "There are some things you learn best in calm, and some in storm.",
+    book: "Willa Cather",
+    author: "The Song of the Lark",
+  },
+  {
+    line: "When you play the game of thrones you win or you die.",
+    book: " George R. R. Martin",
+    author: "A Game of Thrones",
+  },
+  {
+    line: "The world breaks everyone, and afterward, many are strong at the broken places.",
+    book: "Ernest Hemingway",
+    author: "A Farewell to Arms",
+  },
+  {
+    line: "From that time on, the world was hers for the reading. She would never be lonely again, never miss the lack of intimate friends. Books became her friends and there was one for every mood.",
+    book: "Betty Smith",
+    author: "A Tree Grows in Brooklyn",
+  },
+  {
+    line: "Once upon a time there was a boy who loved a girl, and her laughter was a question he wanted to spend his whole life answering.",
+    book: "Nicole Krauss",
+    author: "The History of Love",
+  },
+  {
+    line: "Anyone who ever gave you confidence, you owe them a lot.",
+    book: "Truman Capote",
+    author: "Breakfast at Tiffany’s",
+  },
+  {
+    line: "Isn’t it nice to think that tomorrow is a new day with no mistakes in it yet?",
+    book: "L. M. Montgomery",
+    author: "Anne of Green Gables",
+  },
+  {
+    line: "Call me Ishmael.",
+    book: " Herman Melville",
+    author: "Moby Dick",
+  },
+  {
+    line: "The past is not dead. In fact, it’s not even past.",
+    book: "William Faulkner",
+    author: "Requiem for a Nun",
+  },
+  {
+    line: "He has put a knife on the things that held us together and we have fallen apart.",
+    book: "Chinua Achebe",
+    author: "Things Fall Apart",
+  },
+  {
+    line: "Brave doesn’t mean you’re not scared. It means you go on even though you’re scared.",
+    book: "The Hate U Give",
+    author: "Angie Thomas",
+  },
+  {
+    line: "Great dreamers' dreams are never fulfilled, they are always transcended.",
+    book: "The Monk who sold his Ferrari",
+    author: "Alfred Lord Whitehead",
+  },
+  {
+    line: "Stupidity is one of the two things we see most clearly in retrospect. The other is missed chances.",
+    book: "11/23/63",
+    author: "Stephen King",
+  },
+
+  {
+    line: "Sometimes, you read a book and it fills you with this weird evangelical zeal, and you become convinced that the shattered world will never be put back together unless and until all living humans read the book.",
+    book: "The Fault in Our Stars",
+    author: "John Green",
+  },
+  {
+    line: "If you don't understand, ask questions. If you're uncomfortable about asking questions, say you are uncomfortable about asking questions and then ask anyway.",
+    book: "Americanah",
+    author: "Ngozi Adichie",
+  },
+  {
+    line: "Perhaps one did not want to be loved so much as to be understood.",
+    book: "1984",
+    author: "George Orwell",
+  },
+  {
+    line: "The great thing about this life of ours is that you can be someone different to everybody.",
+    book: "All the Bright Places",
+    author: "Jennifer Niven",
+  },
+  {
+    line: "The moment you doubt whether you can fly, you cease for ever to be able to do it.",
+    book: "Peter Pan",
+    author: "J.M. Barrie",
+  },
+  {
+    line: "It matters not what someone is born, but what they grow to be.",
+    book: "Harry Potter and the Goblet of Fire",
+    author: "J.K. Rowling",
+  },
+  {
+    line: "Whatever our souls are made of, his and mine are the same.",
+    book: "Wuthering Heights",
+    author: "Emily Bronte",
+  },
+  {
+    line: "I am not afraid of storms, for I am learning how to sail my ship",
+    book: "Little Women",
+    author: "Louisa May Alcott",
+  },
+  {
+    line: "There are years that ask questions and years that answer.",
+    book: "Their Eyes Were Watching God",
+    author: "Zora Neale Hurston",
+  },
+  {
+    line: "There is some good in this world and it’s worth fighting for.",
+    book: "The Two Towers",
+    author: "J.R.R. Tolkien",
+  },
+  {
+    line: "Real courage is when you know you’re licked before you begin, but you begin anyway and see it through no matter what.",
+    book: "To Kill a Mockingbird",
+    author: "Harper Lee",
+  },
+  {
+    line: "Whatever our souls are made of, his and mine are the same.",
+    book: "Wuthering Heights",
+    author: "Emily Brontë",
+  },
+  {
+    line: "It's a dangerous business, Frodo, going out your door. You step onto the road, and if you don't keep your feet, there's no knowing where you might be swept off to.",
+    book: "The Lord of the Rings",
+    author: "J.R.R. Tolkien",
+  },
+
+  {
+    line: "Be yourself and people will like you.",
+    book: "Diary of a Wimpy Kid",
+    author: "Jeff Kinney",
+  },
+
+  {
+    line: "The one thing that doesn’t abide by majority rule is a person’s conscience.",
+    book: "To Kill a Mockingbird",
+    author: "Ashley Ross",
+  },
+  {
+    line: "Sitting around miserable all day won't make you any happier.",
+    book: "The Boy in the Striped Pyjamas",
+    author: "John Boyne",
+  },
+  {
+    line: "We need never be ashamed of our tears.",
+    book: "Great Expectations",
+    author: "Charles Dickens",
+  },
+  {
+    line: "I don't understand it any more than you do, but one thing I've learned is that you don't have to understand things for them to be.",
+    book: "A Wrinkle in Time",
+    author: "Madeleine L'Engle",
+  },
+  {
+    line: "There is nothing sweeter in this sad world than the sound of someone you love calling your name.",
+    book: "The Tale of Despereaux",
+    author: "Kate DiCamillo",
+  },
+  {
+    line: "It's the possibility of having a dream come true that makes life interesting.",
+    book: "The Alchemist",
+    author: "Paulo Coelho",
+  },
+];
